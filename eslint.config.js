38d44418447b3fee994eb22d83ddeb55c1a0ce50
Shane Friedman <smoores-gpg@friedmans.us>
2025-03-03 14:53:50 -0500

    Add initial configuration

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { fixStyle: "inline-type-imports" },
+      ],
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: [
+            "eslint.config.js",
+            "vite.config.ts",
+            "lint-staged.config.js",
+            "demo/main.tsx",
+          ],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
---
+++ lint-staged.config.js
@@ -0,0 +1,24 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  "*.{tsx,ts,js,cjs,json}": ["eslint --fix", "prettier --write"],
+  // If typescript files or json files (Typescript statically types .json
+  // files, and package.json and tsconfig.json files can change type
+  // correctness) change, we run tsc on the whole project. We use
+  // incremental: true in our tsconfig, so this isn't very expensive if
+  // only a few files have changed.
+  //
+  // Note that we use the function configuration option here, instead of
+  // just a string or array of strings. lint-staged calls this function
+  // with an array of filenames and expects us to produce an entire command
+  // (including filename arguments). Since we just want to run check:types
+  // on the whole project, not some specific files, we ignore this file list.
+  "*.{tsx,ts,json}": () => "yarn check:types",
+  // Keep the table of contents up to date in the README file
+  "README.md": () => "yarn fix:toc",
+  // For markdown, HTML, and YAML files, we just run Prettier. ESLint doesn't have
+  // anything to say about these.
+  "*.{md,yml,html}": "prettier --write",
+};
